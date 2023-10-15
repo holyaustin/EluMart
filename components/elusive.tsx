67baa4d9dfe9e501ddb28672048e3f459ae711fa
holyaustin <holyaustin@yahoo.com>
2023-10-15 23:57:55 +0100

    night

--- components/elusive.tsx
+++ components/elusive.tsx
@@ -1,9 +1,5 @@
-import { SystemProgram, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
-import { Network, Nft } from "@/types"
-import { AspectRatio } from "@/components/ui/aspect-ratio"
-import { Skeleton } from "@/components/ui/skeleton"
-import { Typography } from "@/components/ui/typography"
 import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { useWallet } from "@solana/wallet-adapter-react";
 import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,24 +9,10 @@ import { getParams } from "./helpers";
 import { Elusiv, TokenType } from "@elusiv/sdk";
 ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m));
 
-type NFTItemProps = {
-  nft: Nft
-  network: Network
-}
-const responsiveIframe = {
-  position: "absolute",
-  top: "0",
-  left: "0",
-  bottom: "0",
-  right: "0",
-  width: "100%",
-  height: "100%",
-} as const;
-
 // Add a recipient public key here
 const recipient = new PublicKey("6UadoV4WxF64tsSpCcHzCZEbjFxPCoogx4wbESywcsxM");
 
-export function NFTItem({ nft, network }: NFTItemProps) {
+const App = () => {
   const [balance, setBalance] = useState(BigInt(0));
   const [isLoading, setIsLoading] = useState(true);
   const [elusiv, setElusiv] = useState<Elusiv>(null);
@@ -39,14 +21,35 @@ export function NFTItem({ nft, network }: NFTItemProps) {
   const [connection, setConnection] = useState(null);
   const [isSending, setIsSending] = useState(false);
 
+  const wallet = useWallet();
+  async function walletAddress() {
+    console.log("Wallet Address is :")
+    try
+    {
+      if (wallet.connected)
+      {
+        const pkk = wallet.publicKey?.toBase58();
+        console.log("Wallet Address is :", pkk)
+      }
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
+  
+
   useEffect(() => {
+    console.log("Use effect2");
     const setParams = async () => {
+      await walletAddress();
       const { elusiv: e, keyPair: kp, connection: conn } = await getParams();
       setElusiv(e);
       setKeyPair(kp);
       setConnection(conn);
-	  // toast.info("Instantiated Elusiv");
+	  toast.info("Instantiated Elusiv");
       setIsLoading(false);
+
+    
     };
 
     setParams();
@@ -59,7 +62,6 @@ export function NFTItem({ nft, network }: NFTItemProps) {
     tokenType: TokenType
   ) => {
     // Build our topup transaction
-    console.log("Top Transaction called");
     const topupTx = await elusivInstance.buildTopUpTx(amount, tokenType);
     // Sign it (only needed for topups, as we're topping up from our public key there)
     topupTx.tx.partialSign(keyPair);
@@ -74,7 +76,6 @@ export function NFTItem({ nft, network }: NFTItemProps) {
     tokenType: TokenType
   ) => {
     // Build the send transaction
-    console.log("send Transaction called");
     const sendTx = await elusivInstance.buildSendTx(
       amount,
       recipient,
@@ -85,12 +86,12 @@ export function NFTItem({ nft, network }: NFTItemProps) {
   };
 
   useEffect(() => {
+    console.log("Use effect2");
+    walletAddress();
     const getBalance = async () => {
-      //toast.info("Fetching private balance...");
-      console.log("Fetching private balance...");
+	  toast.info("Fetching private balance...");
       const privateBalance = await elusiv.getLatestPrivateBalance("LAMPORTS");
-      //toast.success("Fetched private balance!");
-      console.log("Fetched private balance!");
+	  toast.success("Fetched private balance!");
       setBalance(privateBalance);
       setFetching(false);
     };
@@ -102,16 +103,14 @@ export function NFTItem({ nft, network }: NFTItemProps) {
 
   const topupHandler = async (e) => {
     e.preventDefault();
-    //toast.info("Initiating topup...");
-    //console.log("Topup handler");
+    toast.info("Initiating topup...");
     const sig = await topup(
       elusiv,
       keyPair,
       LAMPORTS_PER_SOL,
       "LAMPORTS"
     );
-    // toast.success(`Topup complete with sig ${sig.signature}`);
-    console.log(`Topup complete with sig ${sig.signature}`);
+    toast.success(`Topup complete with sig ${sig.signature}`);
   };
 
   const sendHandler = async (e) => {
@@ -130,46 +129,9 @@ export function NFTItem({ nft, network }: NFTItemProps) {
 	}
   };
 
-
- return (
+  return (
     <>
-
-      <div className="overflow-hidden rounded-2xl bg-white shadow-card">
-      <a
-      href={`https://translator.shyft.to/address/${nft.mint}?cluster=${network}&compressed=true`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-        <AspectRatio>
-          <iframe 
-            title={nft.name}
-            style={responsiveIframe} 
-            src={nft.cached_image_uri ?? nft.image_uri}
-            className="w-full h-auto object-contain aspect-video" 
-            //alt={nft.name} 
-            />
-        </AspectRatio>
-        
-
-        <div className="p-5 w-full">
-          <Typography className="mb-2 font-semibold">{nft.name}</Typography>
-          <Typography as="p" color="secondary" level="body4" className="line-clamp-2 text-ellipsis">
-            {nft.description}
-          </Typography>
-          <Typography className="mb-2 font-semibold">{nft.royalty}  SOL</Typography>
-
-        </div>
-        </a>
-        <button
-              type="button"
-          className="flex flex-row justify-center items-center  w-full my-2 bg-red-500 p-2 rounded-full cursor-pointer hover:bg-yellow-500 hover:text-white border-2 border-x-red-500 text-xl"
-          onClick={(e) => sendHandler(e)} disabled={isLoading || balance <= 0 || isSending}
-            >
-              Collect
-       </button>
-       <ToastContainer autoClose={5000} />
-       
-       {/**
+      <h1>Elusiv Demo</h1>
       <p>
         Connected to {isLoading ? "Loading..." : keyPair?.publicKey.toString()}
       </p>
@@ -182,25 +144,10 @@ export function NFTItem({ nft, network }: NFTItemProps) {
       </button> {" "}
       <button onClick={(e) => sendHandler(e)} disabled={isLoading || balance <= 0 || isSending}>
         Send
-       </button>
-        */}
-	
-
-      </div>
+      </button>
+	  <ToastContainer autoClose={5000} />
     </>
-  )
-}
+  );
+};
 
-export function NFTItemSkeleton() {
-  return (
-    <div className="overflow-hidden rounded-2xl bg-white shadow-card">
-      <AspectRatio>
-        <Skeleton className="h-full w-full" />
-      </AspectRatio>
-      <div className="p-5">
-        <Skeleton className="mb-2 h-5 w-1/3" />
-        <Skeleton className="h-4 w-1/2" />
-      </div>
-    </div>
-  )
-}
+export default App;
---
+++ pages/helpers.ts
@@ -0,0 +1,42 @@
+import { Elusiv, SEED_MESSAGE } from "@elusiv/sdk";
+import { sign } from "@noble/ed25519";
+import { Connection, Keypair } from "@solana/web3.js";
+
+export async function getParams(): Promise<{
+  elusiv: Elusiv;
+  keyPair: Keypair;
+  connection: Connection;
+}> {
+  const connection = new Connection("https://api.devnet.solana.com");
+  // Add your own private key here
+  const keyPair = Keypair.fromSecretKey(
+    new Uint8Array([
+      202, 171, 192, 129, 150, 189, 204, 241, 142, 71, 205, 2, 81, 97, 2, 176, 48,
+  81, 45, 1, 96, 138, 220, 132, 231, 131, 120, 77, 66, 40, 97, 172, 91, 245, 84,
+  221, 157, 190, 9, 145, 176, 130, 25, 43, 72, 107, 190, 229, 75, 88, 191, 136,
+  7, 167, 109, 91, 170, 164, 186, 15, 142, 36, 12, 23,
+    ])
+  );
+  const seed = getSignedSeed(keyPair);
+  console.log(seed);
+
+  const elusiv = await Elusiv.getElusivInstance(
+    seed,
+    keyPair.publicKey,
+    connection,
+    "devnet"
+  );
+
+  return {
+    elusiv,
+    keyPair,
+    connection,
+  };
+}
+
+function getSignedSeed(keyPair: Keypair) {
+    return sign(
+      Buffer.from(SEED_MESSAGE, "utf-8"),
+      keyPair.secretKey.slice(0, 32)
+    );
+};
